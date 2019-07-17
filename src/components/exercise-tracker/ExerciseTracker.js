15e2b6d61ea2df9e1ef77f72217ed355c29b1841
3mpirical <3mpirical10510@gmail.com>
2019-07-17 01:48:34 -0600

    refactored test code to newer test code that retrieves training_date based on the date set, and then retrieves the repSchemes once we have our date

--- src/components/exercise-tracker/ExerciseTracker.js
+++ src/components/exercise-tracker/ExerciseTracker.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect, useContext } from "react";
+import moment from "moment";
 import axios from "axios";
 import { AuthContext } from "../../state/AuthContext";
 
 
 const ExerciseTracker = ({  }) => {
     const { user } = useContext(AuthContext);
-    const [trainingDates, setTrainingDates] = useState([]);
+    const [currentDate, setCurrentDate] = useState(null);
+    const [repSchemes, setRepSchemes] = useState([]);
 
     useEffect(() => {
-        axios.get(`/api/users/${user.id}/training_dates`)
-        .then((res) => setTrainingDates(res.data))
-        .catch(console.log)
+        axios.get(`/api/users/${user.id}/training_dates/get_by_date/${moment().format('YYYY-MM-DD')}`)
+        .then((res) => setCurrentDate(res.data))
+        .catch(console.log);
     }, []);
 
-    const renderDates = () => {
-        return trainingDates.map((trainingDate) => (
-            <p key={trainingDate.id} >{ trainingDate.date }</p>
-        ))
-    }
+    useEffect(() => {
+        if(currentDate) {
+            axios.get(`/api/training_dates/${currentDate.id}/rep_schemes`)
+            .then((res) => setRepSchemes(res.data))
+            .catch(console.log);
+        }
+    }, [currentDate]);
 
+    const renderRepScemes = () => (
+        repSchemes.map(({ comment, exercise_name, id }) => (
+            <div className="rep-scheme" key={id} >
+                <h3>{ exercise_name }</h3>
+                <p>{ comment }</p>
+                <hr/>
+            </div>
+        ))
+    );
 
     return (
         <main className="tracker">
-            { trainingDates && renderDates() }
+            { currentDate && <><h1>{ currentDate.date }</h1><br/><hr /></> }
+            { repSchemes && renderRepScemes() }
         </main>
     )
 }
