ff18cd4eded76b36a0e3a26df40b4999724e9125
3mpirical <3mpirical10510@gmail.com>
2019-07-16 22:03:47 -0600

    created global styles and refactored scss to remove duplicate styles

--- src/components/exercise-tracker/ExerciseTracker.js
+++ src/components/exercise-tracker/ExerciseTracker.js
@@ -4,7 +4,17 @@ import React, { useState } from "react";
 const ExerciseTracker = ({  }) => {
     return (
         <main className="tracker">
-            <h3 style={{textAlign: "center"}} >TRACKER GOES HERE</h3>
+            <h1 style={{textAlign: "center"}} >Tracker Goes Here</h1>
+            <h2 style={{textAlign: "center"}} >Tracker Goes Here</h2>
+            <h3 style={{textAlign: "center"}} >Tracker Goes Here</h3>
+            <h4 style={{textAlign: "center"}} >Tracker Goes Here</h4>
+            <h5 style={{textAlign: "center"}} >Tracker Goes Here</h5>
+            <h6 style={{textAlign: "center"}} >Tracker Goes Here</h6>
+            <input type="text" placeholder="placeholder" />
+            <br/>
+            <button>button</button>
+            <br/>
+            <a href="lsdjflksjd">anchor tag</a>
         </main>
     )
 }
