0131c633224d609493ddec58da1c973309c152b0
3mpirical <3mpirical10510@gmail.com>
2019-07-17 16:25:26 -0600

    exercise search bar now properly sets time out for eventual axios request

--- src/components/exercise-tracker/ExerciseSearch.js
+++ src/components/exercise-tracker/ExerciseSearch.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 
 const ExerciseSearch = () => {
+    const [term, setTerm] = useState("");
+    const [timer, setTimer] = useState(null);
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        clearTimeout(timer);
+
+        setTimer(setTimeout(function () {
+            console.log(term);
+        }, 500));
+    }, [term]);
+
     return (
         <div className="exercise-search">
+            <input 
+                type="text" 
+                className="exercise-search__search-input"
+                placeholder="search exercises ..."
+                value={term}
+                name="term"
+                onChange={handleChange}
+            />
 
         </div>
     );
