afc64730c7355ed0f3a6eee3f917042819538089
3mpirical <3mpirical10510@gmail.com>
2019-07-21 17:16:31 -0600

    updated navbar button styles with icons

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,10 @@ import React, { useContext } from "react";
 import { AuthContext } from "../../state/AuthContext";
 import { withRouter } from "react-router-dom";
 import { FaCommentDots } from "react-icons/fa";
+import { FaBolt } from "react-icons/fa";
+import { FaColumns } from "react-icons/fa";
+import { FaProjectDiagram } from "react-icons/fa";
+
 
 const Navbar = ({ history }) => {
     const auth = useContext(AuthContext);
@@ -11,13 +15,13 @@ const Navbar = ({ history }) => {
             <div className="navbar__left">
                 { auth.authenticated 
                 && <button 
-                    className="navbar__button" 
+                    className="navbar__button-left" 
                     onClick={() => auth.handleLogout(history)}>
                     Logout
                   </button>
                 }
                 { auth.authenticated
-                && <button className="navbar__button" >
+                && <button className="navbar__button-left" >
                     { auth.user.name }
                   </button>
                 }
@@ -26,17 +30,20 @@ const Navbar = ({ history }) => {
             <p className="navbar__logo">Liftr</p>
 
             <div className="navbar__right">
-                <button className="navbar__button">
+                <button className="navbar__button-right">
                     Tracker
+                    <FaProjectDiagram className="navbar__button-icon" />
                 </button>
-                <button className="navbar__button">
+                <button className="navbar__button-right">
                     Challenges
+                    <FaBolt className="navbar__button-icon" />
                 </button>
-                <button className="navbar__button">
+                <button className="navbar__button-right">
                     Boards
+                    <FaColumns className="navbar__button-icon" />
                 </button>
-                <button className="navbar__icon">
-                    <FaCommentDots />
+                <button className="navbar__button-right">
+                    <FaCommentDots className="navbar__message-icon" />
                 </button>
             </div>
         </nav>
